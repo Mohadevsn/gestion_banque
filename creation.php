d1d7dce30325fd75651ad44442a242f1c090a333
Mohadevsn <mohawade23@gmail.com>
2023-12-14 21:46:13 +0000

    creation and lecture done

---
+++ creation.php
@@ -0,0 +1,78 @@
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1 class="title">Bank of Liberty</h1>
+    <nav>
+        <ul>
+            <li><a href="creation.php">Creation de compte</a></li>
+            <li><a href="lecture.php">Liste des clients</a></li>
+            <li><a href="">Mettre a jour le solde</a></li>
+            <li><a href="">Supprimer un client</a></li>
+        </ul>
+    </nav>
+
+        <div class="container">
+            <?php
+            if(isset($_POST["submit"])){
+                $prenom = $_POST["prenom"];
+                $nom = $_POST["nom"];
+                $code = $_POST["code"];
+                $numcompte = $_POST["numerocompte"];
+                $solde = $_POST["solde"];
+                $error = array();
+                if(empty($prenom)){
+                    array_push($error, "Le prenom n'est pas renseigner");
+                }
+                if(empty($nom)){
+                    array_push($error, "Le nom n'est pas renseigner");
+                }
+                if(empty($code)){
+                    array_push($error, "Le code n'est pas renseigner");
+                }
+                if(empty($numcompte)){
+                    array_push($error, "Le numero de compte n'est pas renseigner");
+                }
+                if(empty($solde)){
+                    array_push($error, "Le solde n'est pas renseigner");
+                }
+            
+            
+                //let's connect to the database
+                if(count($error) > 0){
+                    foreach($error as $errors){
+                        echo "<p class=\"fail\">$errors</p>";
+                    }
+                }
+                else{
+                    $conn = mysqli_connect("localhost", "root", "root", "banqueofliberty");
+                    if (!$conn) {
+                        die("Connection failed: " . mysqli_connect_error());
+                    }
+                    //insertion of data into the table clients
+                    $sql = "INSERT INTO client (prenom, nom, code, numcompte, solde) VALUES ('$prenom', '$nom', $code, $numcompte, $solde);";
+                    $query = mysqli_query($conn, $sql); // corrected mysqli_query parameter order
+                    if(!$query){
+                        echo 'Erreur lors de l\'ajout du client: ' . mysqli_error($conn);
+                    } else {
+                        echo "<p class=\"success\">Ajout réussi</p>";
+                    }
+            
+                    // close the database connection
+                    mysqli_close($conn);
+                }
+            
+            }
+            ?>
+            <form action="creation.php" method="POST">
+                Prenom:<br></Prenom:br><input type="text" name="prenom"><br>
+                Nom:<br><input type="text" name="nom"><br>
+                code:<br><input type="number" name="code"><br>
+                Numero de compte:<br><input type="number" name="numerocompte"><br>
+                Solde:<br><input type="number" name="solde"><br>
+                <input type="submit" value="Envoyer" name="submit" class="submit"><br>
+            </form>
+        </div>
+</body>
+</html>
---
+++ lecture.php
@@ -0,0 +1,43 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+
+    </head>
+    <body>
+        <h1 class="title">Bank of Liberty</h1>
+        <nav>
+            <ul>
+                <li><a href="creation.php">Creation de compte</a></li>
+                <li><a href="lecture.php">Liste des clients</a></li>
+                <li><a href="">Mettre a jour le solde</a></li>
+                <li><a href="">Supprimer un client</a></li>
+            </ul>
+        </nav>
+
+        <?php 
+
+            //let's connect to the database
+
+            $conn = mysqli_connect("localhost", "root", "root", "banqueofliberty");
+            $sql = "SELECT * from client";
+            $result = mysqli_query($conn, $sql);
+            $tab = mysqli_fetch_all($result);
+
+            $chaine = "<div class=\"table\"><table border='1px' ><tr><td>Prenom</td><td>Nom</td><td>Code</td><td>Numero de compte</td><td>Solde</td></tr>";
+
+            foreach($tab as $line){
+                $prenom = $line[1];
+                $nom = $line[2];
+                $code = $line[3];
+                $numcompte = $line[4];
+                $solde = $line[5];
+                
+                $chaine = $chaine."<tr><td>$prenom</td><td>$nom</td><td>$code</td><td>$numcompte</td><td>$solde</td></tr>";
+            }
+            $chaine = $chaine."</table></div>";
+
+            print($chaine);
+
+        ?>
+    </body>
+</html>
